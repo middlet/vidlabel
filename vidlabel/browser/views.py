4d41d0e5013eeb55fe3286c389e00d277ab48bd4
lee <ljm@it-innovation.soton.ac.uk>
2014-03-14 17:28:49 +0000

    added dialogues for number of files

--- vidlabel/browser/views.py
+++ vidlabel/browser/views.py
@@ -1,4 +1,19 @@
 from django.views.generic import TemplateView
+from django.conf import settings
+
+import os
+
+
 
 class BrowserView(TemplateView):
-	template_name = 'browser.html'
+    template_name = 'browser.html'
+
+    def get_context_data(self, **kwargs):
+        dirs = os.listdir(os.path.join(settings.STATICFILES_DIRS[0], "data"))
+        files  = []
+        for di in dirs:
+            files.append(len(os.listdir(os.path.join(settings.STATICFILES_DIRS[0], "data", di))))
+
+        return {'directories':dirs, 'nfiles': files}
+
+
